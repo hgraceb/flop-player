ff862cb621375b049caac4917f2170332aedbdf3
Flop <kukuzuo@gmail.com>
2021-06-12 11:49:12 +0800

    feat: 新增title多语言处理

--- src/js/i18n.js
+++ src/js/i18n.js
@@ -1,7 +1,34 @@
-export let i18n;
+let i18n;
 
-if (document.documentElement.lang && document.documentElement.lang.startsWith('en')) {
-    i18n = require('../i18n/en.json');
+if (parent.document.documentElement.lang && parent.document.documentElement.lang.startsWith('en')) {
+    i18n = new function () {
+        this.videoReplay = "Replay"
+        this.videoPause = "Pause"
+        this.videoExit = "Exit"
+        this.videoClose = this.videoExit
+        this.speedModify = "Modify speed"
+        this.speedReset = "Click Reset"
+        this.rateModify = "Modify progress"
+        this.videoTime = "Time"
+    };
 } else {
-    i18n = require('../i18n/zh.json');
+    i18n = new function () {
+        this.videoReplay = "重新播放"
+        this.videoPause = "暂停播放"
+        this.videoExit = "退出播放"
+        this.videoClose = this.videoExit
+        this.speedModify = "速度调节"
+        this.speedReset = "点击复原"
+        this.rateModify = "进度调节"
+        this.videoTime = "时间"
+    };
 }
+
+$("#videoReplay").attr("title", i18n.videoReplay)
+$("#videoPause").attr("title", i18n.videoPause)
+$("#videoExit").attr("title", i18n.videoExit)
+$("#videoClose").attr("title", i18n.videoClose)
+$("#range_speed").attr("title", i18n.speedModify)
+$("#speed_value").attr("title", i18n.speedReset)
+$("#range_rate").attr("title", i18n.rateModify)
+$("#rate_value").attr("title", i18n.videoTime)
