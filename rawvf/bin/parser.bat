716d25453b564cddd221776764a68eb857008f67
Flop <kukuzuo@gmail.com>
2021-06-13 19:48:09 +0800

    chore: 修改exe文件后缀名，在文件名中中间保留exe

--- rawvf/bin/parser.bat
+++ rawvf/bin/parser.bat
@@ -3,37 +3,37 @@
 for %%a in (*.avf) do (
    set str=%%a
    set var=!str:~0,-4!
-   parser_avf.flop "%%a" echo > !var!.rawvf
+   parser_avf.exe.flop "%%a" echo > !var!.rawvf
 )
 
 for %%a in (*.fsvf) do (
    set str=%%a
    set var=!str:~0,-5!
-   parser_fsvf.flop "%%a" echo > !var!.rawvf
+   parser_fsvf.exe.flop "%%a" echo > !var!.rawvf
 )
 
 for %%a in (*.mvf) do (
    set str=%%a
    set var=!str:~0,-4!
-   parser_mvf.flop "%%a" > !var!.rawvf
+   parser_mvf.exe.flop "%%a" > !var!.rawvf
 )
 
 for %%a in (*.mvr) do (
    set str=%%a
    set var=!str:~0,-4!
-   parser_mvr.flop "%%a" > !var!.rawvf
+   parser_mvr.exe.flop "%%a" > !var!.rawvf
 )
 
 for %%a in (*.rmv) do (
    set str=%%a
    set var=!str:~0,-4!
-   parser_rmv.flop "%%a" > !var!.rawvf
+   parser_rmv.exe.flop "%%a" > !var!.rawvf
 )
 
 for %%a in (*.umf) do (
    set str=%%a
    set var=!str:~0,-4!
-   parser_umf.flop "%%a" > !var!.rawvf
+   parser_umf.exe.flop "%%a" > !var!.rawvf
 )
 
 for %%a in (*.rawvf.rawvf) do (
@@ -43,7 +43,7 @@ for %%a in (*.rawvf.rawvf) do (
 for %%a in (*.rawvf) do (
    set str=%%a
    set var=!str:~0,-6!
-   parser_raw.flop "%%a" > !var!.rawvf.rawvf
+   parser_raw.exe.flop "%%a" > !var!.rawvf.rawvf
 )
 
 exit
