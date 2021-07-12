79feda6fa8f53cb672ac7ada82bf688681dd525f
Flop <kukuzuo@gmail.com>
2021-07-12 19:50:01 +0800

    feat: 新增Vuex插件

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import './styles/common.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import store from './store'
 
-createApp(App).mount('#app')
+createApp(App).use(store).mount('#app')
