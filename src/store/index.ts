c5119a7b3b96d20447b78764a42e6a81084df8a4
Flop <kukuzuo@gmail.com>
2021-07-17 14:41:11 +0800

    feat: 在生产环境启用Vuex的严格模式

--- src/store/index.ts
+++ src/store/index.ts
@@ -18,5 +18,7 @@ export type VuexStore = Omit<Store<State>, 'getters' | 'commit'> & {
 export const store: VuexStore = createStore({
   state,
   getters,
-  mutations
+  mutations,
+  // 在严格模式下，无论何时发生了状态变更且不是由 mutation 函数引起的，将会抛出错误。这能保证所有的状态变更都能被调试工具跟踪到。
+  strict: process.env.NODE_ENV !== 'production'
 })
