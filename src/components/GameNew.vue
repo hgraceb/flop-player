629c5617a8ef9eff1e21a93f405658dcee91c309
Flop <kukuzuo@gmail.com>
2021-08-09 00:41:19 +0800

    feat: 新增游戏顶部计数器

--- src/components/GameNew.vue
+++ src/components/GameNew.vue
@@ -5,6 +5,8 @@
   <base-svg :height="height" :width="width">
     <skin-border-top />
     <skin-border-upper />
+    <skin-counter-top :translate-x="minesCountTranslateX" />
+    <skin-counter-top :translate-x="timeCountTranslateX" />
     <skin-border-middle />
     <skin-border-lower />
     <skin-border-bottom />
@@ -30,9 +32,11 @@ import { computed, defineComponent } from 'vue'
 import SkinBorderMiddle from '@/components/skin/SkinBorderMiddle.vue'
 import SkinBorderLower from '@/components/skin/SkinBorderLower.vue'
 import SkinBorderBottom from '@/components/skin/SkinBorderBottom.vue'
+import SkinCounterTop from '@/components/skin/SkinCounterTop.vue'
 
 export default defineComponent({
   components: {
+    SkinCounterTop,
     SkinBorderBottom,
     SkinBorderLower,
     SkinBorderMiddle,
@@ -48,7 +52,13 @@ export default defineComponent({
     const height = computed(() => {
       return (SIZE_BORDER_TOP.height + SIZE_BORDER_UPPER.height + SIZE_BORDER_MIDDLE.height + store.state.height * SIZE_BORDER_LOWER.height * SIZE_CELL.height + SIZE_BORDER_BOTTOM.height) * SVG_SCALE
     })
-    return { width, height }
+    // 雷数计数器的 X 轴偏移量，3 为雷数计数器与左边框的距离
+    const minesCountTranslateX = (SIZE_BORDER_UPPER.width + 3) * SVG_SCALE
+    // 时间计数器的 X 轴偏移量，41 为时间计数器的背景宽度，3 为时间计数器与有边框的距离，Minesweeper X 和 Arbiter 中的值均为 6，看着不爽就改成对称的了
+    const timeCountTranslateX = computed(() => {
+      return (SIZE_BORDER_UPPER.width + store.state.width * SIZE_CELL.width - 41 - 3) * SVG_SCALE
+    })
+    return { width, height, minesCountTranslateX, timeCountTranslateX }
   }
 })
 </script>
---
+++ src/components/skin/SkinCounterTop.vue
@@ -0,0 +1,37 @@
+<template>
+  <g :transform="`translate(${translateX} ${translateY})`">
+    <skin-symbol :translate-x="0" :translate-y="0" name="top-counters" />
+    <skin-symbol :translate-x="firstTranslateX" :translate-y="countTranslateY" name="count-0" />
+    <skin-symbol :translate-x="secondTranslateX" :translate-y="countTranslateY" name="count-0" />
+    <skin-symbol :translate-x="thirdTranslateX" :translate-y="countTranslateY" name="count-0" />
+  </g>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import SkinSymbol from '@/components/skin/SkinSymbol.vue'
+import { SIZE_BORDER_TOP, SVG_SCALE } from '@/game/constants'
+
+export default defineComponent({
+  components: { SkinSymbol },
+  props: {
+    translateX: {
+      type: Number,
+      required: true
+    }
+  },
+  setup () {
+    // 计数器整体的 Y 轴偏移量
+    const translateY = (SIZE_BORDER_TOP.height + 4) * SVG_SCALE
+    // 计数器的数字的 Y 轴偏移量
+    const countTranslateY = 2 * SVG_SCALE
+    // 计数器第一个数字的 X 轴偏移量，2 为数字的左边距
+    const firstTranslateX = 2 * SVG_SCALE
+    // 计数器第二个数字的 X 轴偏移量，11 为单个数字的宽度
+    const secondTranslateX = (2 * 2 + 11) * SVG_SCALE
+    // 计数器第三个数字的 X 轴偏移量
+    const thirdTranslateX = (2 * 3 + 11 * 2) * SVG_SCALE
+    return { translateY, countTranslateY, firstTranslateX, secondTranslateX, thirdTranslateX }
+  }
+})
+</script>
