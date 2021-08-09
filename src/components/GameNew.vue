f3f7cff81df095608550783f921ab6839512ac7e
胡恩彬 <huenbin@dianchu.com>
2021-08-09 13:00:01 +0800

    feat: 新增顶部时间计数器响应式显示

--- src/components/GameNew.vue
+++ src/components/GameNew.vue
@@ -5,8 +5,8 @@
   <base-svg :height="height" :width="width">
     <skin-border-top />
     <skin-border-upper />
-    <skin-counter-top :translate-x="minesCountTranslateX" />
-    <skin-counter-top :translate-x="timeCountTranslateX" />
+    <skin-counter-top :count="10" :translate-x="minesCountTranslateX" />
+    <skin-counter-top :count="countTime" :min="0" :translate-x="timeCountTranslateX" />
     <skin-border-middle />
     <skin-border-lower />
     <skin-border-bottom />
@@ -17,15 +17,7 @@
 import BaseSvg from '@/components/BaseSvg.vue'
 import SkinSprites from '@/components/skin/SkinSprites.vue'
 import SkinBorderTop from '@/components/skin/SkinBorderTop.vue'
-import {
-  SIZE_BORDER_BOTTOM,
-  SIZE_BORDER_LOWER,
-  SIZE_BORDER_MIDDLE,
-  SIZE_BORDER_TOP,
-  SIZE_BORDER_UPPER,
-  SIZE_CELL,
-  SVG_SCALE
-} from '@/game/constants'
+import { SIZE_BORDER_BOTTOM, SIZE_BORDER_LOWER, SIZE_BORDER_MIDDLE, SIZE_BORDER_TOP, SIZE_BORDER_UPPER, SIZE_CELL, SVG_SCALE } from '@/game/constants'
 import { store } from '@/store'
 import SkinBorderUpper from '@/components/skin/SkinBorderUpper.vue'
 import { computed, defineComponent } from 'vue'
@@ -46,19 +38,26 @@ export default defineComponent({
     BaseSvg
   },
   setup () {
+    // SVG 高度信息
     const width = computed(() => {
       return (SIZE_BORDER_TOP.widthLeft + store.state.width * SIZE_BORDER_TOP.widthHorizontal * SIZE_CELL.width + SIZE_BORDER_TOP.widthRight) * SVG_SCALE
     })
+    // SVG 宽度信息
     const height = computed(() => {
       return (SIZE_BORDER_TOP.height + SIZE_BORDER_UPPER.height + SIZE_BORDER_MIDDLE.height + store.state.height * SIZE_BORDER_LOWER.height * SIZE_CELL.height + SIZE_BORDER_BOTTOM.height) * SVG_SCALE
     })
-    // 雷数计数器的 X 轴偏移量，3 为雷数计数器与左边框的距离
+    // 雷数计数器的 X 轴坐标偏移量，3 为雷数计数器与左边框的距离
     const minesCountTranslateX = (SIZE_BORDER_UPPER.width + 3) * SVG_SCALE
-    // 时间计数器的 X 轴偏移量，41 为时间计数器的背景宽度，3 为时间计数器与有边框的距离，Minesweeper X 和 Arbiter 中的值均为 6，看着不爽就改成对称的了
+    // 时间计数器的 X 轴坐标偏移量，41 为时间计数器的背景宽度，3 为时间计数器与有边框的距离，Minesweeper X 和 Arbiter 中的值均为 6，看着不爽就改成对称的了
     const timeCountTranslateX = computed(() => {
       return (SIZE_BORDER_UPPER.width + store.state.width * SIZE_CELL.width - 41 - 3) * SVG_SCALE
     })
-    return { width, height, minesCountTranslateX, timeCountTranslateX }
+    // 当前计数器显示的游戏时间
+    const countTime = computed(() => {
+      // 当游戏经过的时间为 0 时，计数器显示的时间也为 0，否则需要转换成秒数后 +1
+      return store.state.gameElapsedTime === 0 ? 0 : Math.floor(store.state.gameElapsedTime / 1000) + 1
+    })
+    return { width, height, minesCountTranslateX, countTime, timeCountTranslateX }
   }
 })
 </script>
--- src/components/skin/SkinCounterTop.vue
+++ src/components/skin/SkinCounterTop.vue
@@ -1,37 +1,72 @@
 <template>
   <g :transform="`translate(${translateX} ${translateY})`">
     <skin-symbol :translate-x="0" :translate-y="0" name="top-counters" />
-    <skin-symbol :translate-x="firstTranslateX" :translate-y="countTranslateY" name="count-0" />
-    <skin-symbol :translate-x="secondTranslateX" :translate-y="countTranslateY" name="count-0" />
-    <skin-symbol :translate-x="thirdTranslateX" :translate-y="countTranslateY" name="count-0" />
+    <skin-symbol :name="`count-${hun}`" :translate-x="firstTranslateX" :translate-y="countTranslateY" />
+    <skin-symbol :name="`count-${ten}`" :translate-x="secondTranslateX" :translate-y="countTranslateY" />
+    <skin-symbol :name="`count-${one}`" :translate-x="thirdTranslateX" :translate-y="countTranslateY" />
   </g>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { computed, defineComponent } from 'vue'
 import SkinSymbol from '@/components/skin/SkinSymbol.vue'
 import { SIZE_BORDER_TOP, SVG_SCALE } from '@/game/constants'
 
 export default defineComponent({
   components: { SkinSymbol },
   props: {
+    // 计数器整体的 X 轴坐标偏移量
     translateX: {
       type: Number,
       required: true
+    },
+    // 需要显示的值
+    count: {
+      type: Number,
+      required: true
+    },
+    // 最小值
+    min: {
+      type: Number,
+      required: false,
+      default: Number.MIN_SAFE_INTEGER
     }
   },
-  setup () {
-    // 计数器整体的 Y 轴偏移量
+  setup (props) {
+    // 计数器整体的 Y 轴坐标偏移量
     const translateY = (SIZE_BORDER_TOP.height + 4) * SVG_SCALE
-    // 计数器的数字的 Y 轴偏移量
+    // 计数器的数字的 Y 轴坐标偏移量
     const countTranslateY = 2 * SVG_SCALE
-    // 计数器第一个数字的 X 轴偏移量，2 为数字的左边距
+    // 计数器第一个数字的 X 轴坐标偏移量，2 为数字的左边距
     const firstTranslateX = 2 * SVG_SCALE
-    // 计数器第二个数字的 X 轴偏移量，11 为单个数字的宽度
+    // 计数器第二个数字的 X 轴坐标偏移量，11 为单个数字的宽度
     const secondTranslateX = (2 * 2 + 11) * SVG_SCALE
-    // 计数器第三个数字的 X 轴偏移量
+    // 计数器第三个数字的 X 轴坐标偏移量
     const thirdTranslateX = (2 * 3 + 11 * 2) * SVG_SCALE
-    return { translateY, countTranslateY, firstTranslateX, secondTranslateX, thirdTranslateX }
+
+    // 实际显示的值，最大只显示999
+    const value = computed((): number => {
+      return Math.min(Math.max(props.count, props.min), 999)
+    })
+
+    // 百位数的值
+    const hun = computed((): number | string => {
+      // 显示的值为负数时，最多只显示两位数字，百位数显示负号
+      if (value.value < 0) {
+        return 'minus'
+      }
+      return Math.floor(Math.abs(value.value) / 100) % 10
+    })
+    // 十位数的值
+    const ten = computed((): number => {
+      return Math.floor(Math.abs(value.value) / 10) % 10
+    })
+    // 个位数的值
+    const one = computed((): number => {
+      return Math.abs(value.value) % 10
+    })
+
+    return { translateY, countTranslateY, firstTranslateX, secondTranslateX, thirdTranslateX, hun, ten, one }
   }
 })
 </script>
