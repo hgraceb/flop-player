3f6de62d4c022106bcb97560e1f411de7d55f40d
Flop <kukuzuo@gmail.com>
2021-08-21 23:04:01 +0800

    fix: 修复玩家名称显示没有居中的问题

--- src/components/game/GameBottomMiddle.vue
+++ src/components/game/GameBottomMiddle.vue
@@ -3,7 +3,7 @@
   <g>
     <title>{{ player }}</title>
     <!--119.8 为经验值，将页面一直放大后，测试用户选中文本时上下边距是否基本一致-->
-    <text :transform="`translate(${centerTranslateX} ${translateY})`" class="player-name" x="50%" y="119.8">{{ player }}</text>
+    <text :transform="`translate(0 ${translateY})`" class="player-name" x="50%" y="119.8">{{ player }}</text>
   </g>
   <skin-symbol :translate-x="0" :translate-y="translateY" name="game-bottom-middle-left" />
   <skin-symbol :translate-x="rightTranslateX" :translate-y="translateY" name="game-bottom-middle-right" />
