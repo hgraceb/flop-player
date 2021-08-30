ad5d628eb3f30a4db2eb45bff68379533f0a101a
胡恩彬 <huenbin@dianchu.com>
2021-08-30 13:28:57 +0800

    feat: 新增计数器的3BV/s显示

--- src/components/Counters.vue
+++ src/components/Counters.vue
@@ -2,103 +2,63 @@
   <table>
     <tr v-for="key in Object.keys(stats)" :key="key">
       <td>{{ key }}</td>
-      <td>{{ stats[key].value || stats[key].default }}</td>
+      <td>{{ stats[key] }}</td>
     </tr>
   </table>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, ref, Ref, watch } from 'vue'
+import { computed, defineComponent, ref, Ref } from 'vue'
 import { store } from '@/store'
 
 export default defineComponent({
   setup () {
     interface Stat {
-      [key: string]: {
-        default: string,
-        value?: string
-      }
+      [key: string]: string | number
     }
 
+    const time = computed(() => Math.min(store.state.gameEvents[store.state.gameEvents.length - 1]?.time || 0, store.state.gameElapsedTime) / 1000)
+    const bbbv = computed(() => store.state.bbbv)
+    const solvedBbbv = computed(() => store.state.solvedBbbv)
+
     const stats: Ref<Stat> = ref({
-      RTime: {
-        default: '0.00 (0)'
-      },
-      'Est RTime': {
-        default: '* (*)'
-      },
-      '3BV': {
-        default: '*/*'
-      },
-      '3BV/s': {
-        default: '*/*'
-      },
-      ZiNi: {
-        default: '*@*'
-      },
-      'H.ZiNi': {
-        default: '*@*'
-      },
-      Ops: {
-        default: '*/*'
-      },
-      Isls: {
-        default: '0'
-      },
-      Left: {
-        default: '0@0'
-      },
-      Right: {
-        default: '0@0'
-      },
-      Double: {
-        default: '0@0'
-      },
-      Cl: {
-        default: '0@0'
-      },
-      IOE: {
-        default: '*'
-      },
-      ThrP: {
-        default: '*'
-      },
-      Corr: {
-        default: '*'
-      },
-      ZNE: {
-        default: '*'
-      },
-      HZNE: {
-        default: '*'
-      },
-      ZNT: {
-        default: '*'
-      },
-      HZNT: {
-        default: '*'
-      },
-      Path: {
-        default: '0'
-      },
-      Flags: {
-        default: '0'
-      },
-      RQP: {
-        default: '*'
-      },
-      IOS: {
-        default: '*'
-      }
-    })
-
-    watch(computed(() => store.state.gameElapsedTime), () => {
-      const time = Math.min(store.state.gameEvents[store.state.gameEvents.length - 1]?.time || 0, store.state.gameElapsedTime) / 1000
-      stats.value.RTime.value = `${time.toFixed(2)} (${time === 0 ? 0 : Math.floor(time) + 1})`
-    })
-
-    watch(computed(() => store.state.solvedBbbv), () => {
-      stats.value['3BV'].value = `${store.state.solvedBbbv}/${store.state.bbbv}`
+      RTime: computed(() => {
+        if (time.value <= 0) return '0.00 (0)'
+        return `${time.value.toFixed(2)} (${time.value === 0 ? 0 : Math.floor(time.value) + 1})`
+      }),
+      'Est RTime': computed(() => {
+        if (time.value <= 0) return '* (*)'
+        const estRTime = time.value * (bbbv.value / solvedBbbv.value)
+        return `${estRTime.toFixed(2)} (${estRTime === 0 ? 0 : Math.floor(estRTime) + 1})`
+      }),
+      '3BV': computed(() => {
+        if (time.value <= 0) return '*/*'
+        return `${solvedBbbv.value}/${bbbv.value}`
+      }),
+      '3BV/s': computed(() => {
+        if (time.value <= 0) return '*'
+        // TODO 修复和 Arbiter 计算结果不一致的问题
+        return `${(solvedBbbv.value / time.value).toFixed(2)}`
+      }),
+      ZiNi: '*@*',
+      'H.ZiNi': '*@*',
+      Ops: '*/*',
+      Isls: '0',
+      Left: '0@0',
+      Right: '0@0',
+      Double: '0@0',
+      Cl: '0@0',
+      IOE: '*',
+      ThrP: '*',
+      Corr: '*',
+      ZNE: '*',
+      HZNE: '*',
+      ZNT: '*',
+      HZNT: '*',
+      Path: '0',
+      Flags: '0',
+      RQP: '*',
+      IOS: '*'
     })
 
     return { stats }
