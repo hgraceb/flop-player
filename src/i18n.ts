393a0568348a625e6090be611e5717e6ef0bc6b9
Flop <kukuzuo@gmail.com>
2021-09-05 15:58:03 +0800

    refactor: 优化 Vue I18n 配置

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,11 +1,25 @@
 import { createI18n } from 'vue-i18n'
-import en from '@/locales/en.yml'
-import zhHans from '@/locales/zh-Hans.yml'
 
+/**
+ * 加载所有本地化信息
+ */
+function loadLocaleMessages () {
+  const locales = require.context('./locales', true, /[A-Za-z0-9-_,\s]+\.ya?ml$/i)
+  const messages: { [key: string]: any } = {}
+  locales.keys().forEach(key => {
+    const matched = key.match(/([A-Za-z0-9-_]+)\./i)
+    if (matched && matched.length > 1) {
+      const locale = matched[1]
+      messages[locale] = locales(key)
+    }
+  })
+  return messages
+}
+
+/**
+ * 配置 Vue I18n，开发工具参见：https://kazupon.github.io/vue-i18n/guide/tooling.html
+ */
 export const i18n = createI18n({
   locale: 'zh-Hans',
-  messages: {
-    en: en,
-    'zh-Hans': zhHans
-  }
+  messages: loadLocaleMessages()
 })
