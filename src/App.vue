8a56686c9345bcbd2086fdd381b4910e71585f5b
Flop <kukuzuo@gmail.com>
2021-09-08 21:39:31 +0800

    feat: 缩放控制新增多语言文本配置

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,6 @@
     <game />
     <counters />
     <control-bar />
-    <div>
-      <button :disabled="increaseDisabled" @click="increaseScale">放大</button>
-      <button :disabled="decreaseDisabled" @click="decreaseScale">缩小</button>
-      <span>{{ scale }}x（实际{{ scale.toFixed(2).substring(0, 4) }}）</span>
-    </div>
   </div>
 </template>
 
@@ -16,7 +11,6 @@
 import { computed, defineComponent, onMounted } from 'vue'
 import ControlBar from '@/components/ControlBar.vue'
 import { store } from '@/store'
-import { SCALE_ARRAY } from '@/game/constants'
 import Game from '@/components/Game.vue'
 import Counters from '@/components/Counters.vue'
 import GameMenu from '@/components/GameMenu.vue'
@@ -27,17 +21,12 @@ export default defineComponent({
     // 用户设置的缩放比例
     const scale = computed(() => store.state.scale)
 
-    const increaseScale = () => store.commit('setScale', SCALE_ARRAY[SCALE_ARRAY.indexOf(store.state.scale) + 1])
-    const decreaseScale = () => store.commit('setScale', SCALE_ARRAY[SCALE_ARRAY.indexOf(store.state.scale) - 1])
-    const increaseDisabled = computed(() => store.state.scale >= SCALE_ARRAY[SCALE_ARRAY.length - 1])
-    const decreaseDisabled = computed(() => store.state.scale <= SCALE_ARRAY[0])
-
     onMounted(() => {
       // 测试数据
       store.dispatch('fetchVideo', '')
     })
 
-    return { scale, increaseScale, decreaseScale, increaseDisabled, decreaseDisabled }
+    return { scale }
   }
 })
 </script>
--- src/components/GameMenu.vue
+++ src/components/GameMenu.vue
@@ -1,21 +1,36 @@
 <template>
   <button @click="toggleLocales">{{ $t('menu.toggleLanguages') }}</button>
+  <div>
+    <button :disabled="zoomOutDisabled" @click="zoomOutScale">{{ $t('menu.zoomOut') }}</button>
+    <button :disabled="zoomInDisabled" @click="zoomInScale">{{ $t('menu.zoomIn') }}</button>
+    <span>{{ scale }}x</span>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { computed, defineComponent } from 'vue'
 import { useI18n } from 'vue-i18n'
+import { store } from '@/store'
+import { SCALE_ARRAY } from '@/game/constants'
 
 export default defineComponent({
   setup () {
     const { locale, availableLocales } = useI18n()
 
+    // 切换语言
     const toggleLocales = () => {
-      // change to some real logic
-      const locales = availableLocales
-      locale.value = locales[(locales.indexOf(locale.value) + 1) % locales.length]
+      locale.value = availableLocales[(availableLocales.indexOf(locale.value) + 1) % availableLocales.length]
     }
-    return { toggleLocales }
+
+    // 用户设置的缩放比例
+    const scale = computed(() => store.state.scale)
+
+    const zoomOutScale = () => store.commit('setScale', SCALE_ARRAY[SCALE_ARRAY.indexOf(store.state.scale) - 1])
+    const zoomInScale = () => store.commit('setScale', SCALE_ARRAY[SCALE_ARRAY.indexOf(store.state.scale) + 1])
+    const zoomOutDisabled = computed(() => store.state.scale <= SCALE_ARRAY[0])
+    const zoomInDisabled = computed(() => store.state.scale >= SCALE_ARRAY[SCALE_ARRAY.length - 1])
+
+    return { toggleLocales, scale, zoomOutScale, zoomInScale, zoomOutDisabled, zoomInDisabled }
   }
 })
 </script>
