0c50e1d083438f0388348bc3029dfb317d92e0e5
Flop <kukuzuo@gmail.com>
2021-09-13 21:37:52 +0800

    Revert "feat: 按需引入ElementPlus组件库"
    This reverts commit aff35d6a

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,9 @@ import App from './App.vue'
 import { store } from './store'
 import { i18n } from '@/i18n'
 import NP from 'number-precision'
-import installElementPlus from './plugins/element'
 
 // If you want to get rid of XXX is beyond boundary when transfer to integer, the results may not be accurate, use this at the beginning of your app to turn off boundary checking.
 // https://github.com/nefe/number-precision#usage
 NP.enableBoundaryChecking(false) // default param is true
 
-const app = createApp(App)
-installElementPlus(app)
-app.use(store).use(i18n).mount('#app')
+createApp(App).use(store).use(i18n).mount('#app')
