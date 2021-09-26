28423007277d349f7033e4dcbbad52c15202cc7a
Flop <kukuzuo@gmail.com>
2021-09-26 21:20:16 +0800

    feat: 设置背景颜色

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,6 @@ import NP from 'number-precision'
 import installI18n from './plugins/i18n'
 import installAntDesign from './plugins/ant-design-vue'
 
-// 引入测试用样式
-if (process.env.NODE_ENV !== 'production') {
-  require('./styles/test.css')
-}
-
 // If you want to get rid of XXX is beyond boundary when transfer to integer, the results may not be accurate, use this at the beginning of your app to turn off boundary checking.
 // https://github.com/nefe/number-precision#usage
 NP.enableBoundaryChecking(false) // default param is true
