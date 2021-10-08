8730448aaf3da4bb7d2a06e708aa522be0540838
Flop <kukuzuo@gmail.com>
2021-10-08 22:14:07 +0800

    feat: 自动检测玩家姓名的编码格式

--- src/components/game/GameBottomMiddle.vue
+++ src/components/game/GameBottomMiddle.vue
@@ -14,6 +14,7 @@ import { computed, defineComponent } from 'vue'
 import { CELL_SIDE_LENGTH, GAME_BOTTOM_MIDDLE, GAME_BOTTOM_UPPER, GAME_TOP_LOWER, GAME_TOP_MIDDLE, GAME_TOP_UPPER, SVG_SCALE } from '@/game/constants'
 import { store } from '@/store'
 import SkinSymbol from '@/components/skin/SkinSymbol.vue'
+import chardet from 'chardet'
 
 export default defineComponent({
   components: { SkinSymbol },
@@ -29,16 +30,21 @@ export default defineComponent({
     const rightTranslateX = computed(() => {
       return (GAME_BOTTOM_MIDDLE.widthLeft + store.state.width * CELL_SIDE_LENGTH) * SVG_SCALE
     })
+    // 从原始字节数据解码得到的玩家名称
+    const playerDecoded = computed(() => {
+      // 自动检测玩家姓名的编码格式，经过测试 Windows-1252 可以兼容目前较多的已有录像数据，默认使用 Windows-1252 编码格式
+      // TODO 提供更多的编码格式进行自定义选择，可以分类为自动、常用、中文、英语、日语...
+      return new TextDecoder(chardet.detect(store.state.playerArray) || 'Windows-1252').decode(store.state.playerArray).trim()
+    })
     // 玩家名称
     const player = computed(() => {
-      const player = new TextDecoder('windows-1252').decode(store.state.playerArray)
       // 没有玩家姓名信息则默认显示 Anonymous
-      return player?.trim().length > 0 ? player : 'Anonymous'
+      return playerDecoded.value.length > 0 ? playerDecoded.value : 'Anonymous'
     })
     // 玩家名称的文本不透明度
     const opacityPlayer = computed(() => {
       // 没有玩家姓名信息则置灰显示
-      return new TextDecoder('windows-1252').decode(store.state.playerArray)?.trim().length > 0 ? 1 : 0.25
+      return playerDecoded.value.length > 0 ? 1 : 0.25
     })
     return { translateY, centerTranslateX, rightTranslateX, player, opacityPlayer }
   }
