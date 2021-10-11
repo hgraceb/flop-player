c7b37b74e75fb859b2d074a3ec498bf67a5afb12
Flop <kukuzuo@gmail.com>
2021-10-11 22:45:52 +0800

    feat: 布局左右居中

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
-  <!-- TODO 布局居中，解决缩放后没有影响到游戏菜单弹出的窗体内容、 Flex 布局左右边距错误跟随缩放导致有白边的问题 -->
-  <div :style="{transformOrigin: '0 0 0', transform: `scale(${scale})`}" style="display: flex">
-    <div>
-      <counters />
+  <!-- TODO 解决缩放后没有影响到游戏菜单弹出的窗体内容、 Flex 布局左右边距错误跟随缩放导致有白边的问题 -->
+  <div style="width: fit-content;margin-left: auto;margin-right: auto">
+    <div :style="{transformOrigin: '0 0 0', transform: `scale(${scale})`}" style="display: flex;align-items: flex-end">
+      <div style="margin-left: auto">
+        <counters />
+      </div>
+      <div style="margin-right: auto">
+        <game-menu />
+        <game />
+      </div>
     </div>
-    <div>
-      <game-menu />
-      <game />
-      <control-bar />
-    </div>
+    <control-bar style="margin-left: auto;margin-right: auto" />
   </div>
 </template>
 
@@ -29,6 +31,10 @@ export default defineComponent({
     onMounted(() => {
       // TODO 删除测试代码和对应的文件（最后不参与打包或者在 README 文件中说明需要手动删除对应的测试录像文件）
       store.dispatch('fetchVideo', 'videos/arbiter_int.rawvf')
+      // store.dispatch('fetchVideo', 'videos/arbiter_exp.rawvf')
+      // store.dispatch('fetchVideo', 'videos/Cus_8x30_30mines.rawvf')
+      // store.dispatch('fetchVideo', 'videos/Cus_20x20_20mines.rawvf')
+      // store.dispatch('fetchVideo', 'videos/Cus_30x8_30mines.rawvf')
     })
 
     return { scale }
