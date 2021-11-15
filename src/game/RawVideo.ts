7daa382286266ad13aeaa1f096ee52b6a3252229
Flop <kukuzuo@gmail.com>
2021-11-15 20:24:11 +0800

    feat: RawVF 录像解析代码新增获取和校验基本参数的逻辑

--- src/game/RawVideo.ts
+++ src/game/RawVideo.ts
@@ -21,22 +21,30 @@ export class RawVideo extends Video {
       }
       const lineStr = textDecoder.decode(lineArr).toLowerCase().trim()
       const index = lineStr.indexOf(':')
+      // 如果当前行没有 ':' 字符或者 ':' 的位置在首字符，则直接读取下一行
+      if (index <= 0) continue
       const option = lineStr.substring(0, index)
       const value = lineStr.substring(index + 1).trim()
       if (option === 'board') {
-        // 检查是否有必要参数，TODO 移动到下面判断
+        // 检查是否有必要参数
         if (this.mWidth < 0) this.throwError('No width')
         if (this.mHeight < 0) this.throwError('No height')
         if (this.mMines < 0) this.throwError('No mines')
         break
+      } else if (option === 'player') {
+        this.mPlayer = lineArr.slice(index + 1)
       } else if (option === 'width') {
-        // TODO 判断参数合法性
         this.mWidth = Number(value)
+        if (!Number.isInteger(this.mWidth) || this.mWidth <= 0) this.throwError(`Invalid board width: "${value}"`)
       } else if (option === 'height') {
         this.mHeight = Number(value)
+        if (!Number.isInteger(this.mHeight) || this.mHeight <= 0) this.throwError(`Invalid board height: "${value}"`)
       } else if (option === 'mines') {
         this.mMines = Number(value)
-      } else if (option === 'marks') {
+        if (!Number.isInteger(this.mMines) || this.mMines <= 0) this.throwError(`Invalid number of mines: "${value}"`)
+        // QuestionMarks 是为了兼容部分旧版本录像，如：FreeSweeper release 10 保存的录像
+      } else if (option === 'marks' || option === 'QuestionMarks'.toLowerCase()) {
+        if (value !== 'on' && value !== 'off') this.throwError(`Invalid question marks: "${value}"`)
         this.mMarks = value === 'on' ? 1 : 0
       }
     }
