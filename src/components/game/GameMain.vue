495b2ea04da4a756eaa3d00c05e8715cf07941a5
Flop <kukuzuo@gmail.com>
2021-12-01 20:47:51 +0800

    feat: 新增游戏区域的方块点击事件处理

--- src/components/game/GameMain.vue
+++ src/components/game/GameMain.vue
@@ -1,12 +1,15 @@
 <template>
-  <g :transform="`translate(${translateX} ${translateY})`">
-    <!-- 背景颜色，用于修正部分缩放比例下有白边的问题，7.89 是为了填充游戏区域顶部和左侧与边框之前的白边，也是为了尽量不影响正常的图片显示 -->
-    <path :d="`M -7.89 -7.89 h ${gameWidth * 160 + 7.89} v 15.78 h ${gameWidth * -160 + 7.89} v ${gameHeight * 160 - 7.89} h -15.78 Z`" fill="gray" />
+  <!-- 背景颜色，用于修正部分缩放比例下有白边的问题，7.89 是为了填充游戏区域顶部和左侧与边框之前的白边，也是为了尽量不影响正常的图片显示 -->
+  <path :d="`M -7.89 -7.89 h ${gameWidth * 160 + 7.89} v 15.78 h ${gameWidth * -160 + 7.89} v ${gameHeight * 160 - 7.89} h -15.78 Z`" fill="gray" />
+  <!-- 方块容器，除了方块不要放其他元素，避免鼠标事件的坐标位置获取错误 -->
+  <g ref="boardElement" :transform="`translate(${translateX} ${translateY})`">
     <template v-for="(item, height) in gameHeight" :key="item">
       <skin-symbol
         v-for="(item, width) in gameWidth"
         :key="item"
         :name="getCellImg(width, height)"
+        :onmousedown="cellMouseHandler"
+        :onmouseup="cellMouseHandler"
         :translate-x="getTranslateX(width)"
         :translate-y="getTranslateY(height)"
       />
@@ -15,11 +18,12 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from 'vue'
+import { computed, defineComponent, ref } from 'vue'
 import { store } from '@/store'
 import SkinSymbol from '@/components/skin/SkinSymbol.vue'
 import { CELL_SIDE_LENGTH, GAME_MIDDLE, GAME_TOP_LOWER, GAME_TOP_MIDDLE, GAME_TOP_UPPER, SVG_SCALE } from '@/game/constants'
 import { ImgCellType } from '@/util/image'
+import { round } from 'number-precision'
 
 export default defineComponent({
   components: { SkinSymbol },
@@ -48,8 +52,37 @@ export default defineComponent({
     const getTranslateY = (height: number): number => {
       return height * CELL_SIDE_LENGTH * SVG_SCALE
     }
+    // 棋盘对应的元素
+    const boardElement = ref<SVGPolylineElement>()
+    // 处理方块的鼠标事件，TODO 完善鼠标事件处理，除了添加鼠标移动事件还要在 document 上监听鼠标事件
+    const cellMouseHandler = (e: MouseEvent) => {
+      if (!boardElement.value) return
+      // 阻止默认的点击事件执行
+      e.preventDefault()
+      // 阻止捕获和冒泡阶段中当前事件的进一步传播
+      e.stopPropagation()
+      // 获取方块容器的位置信息
+      const rect = boardElement.value.getBoundingClientRect()
+      // 在方块上点击时，限制横坐标最小值为 0，最大值为 width - 1，因为点击边缘位置时可能因为原始数据已经被四舍五入过，导致计算结果有误差
+      const x = Math.max(0, Math.min(round(e.x - rect.x, 0), store.state.width * 16 - 1))
+      // 在方块上点击时，限制纵坐标最小值为 0，最大值为 height - 1
+      const y = Math.max(0, Math.min(round(e.y - rect.y, 0), store.state.height * 16 - 1))
+      if (e.type === 'mousedown' && e.button === 0) {
+        console.log({ type: 'lc', x: x, y: y })
+      } else if (e.type === 'mousedown' && e.button === 1) {
+        console.log({ type: 'mc', x: x, y: y })
+      } else if (e.type === 'mousedown' && e.button === 2) {
+        console.log({ type: 'rc', x: x, y: y })
+      } else if (e.type === 'mouseup' && e.button === 0) {
+        console.log({ type: 'lr', x: x, y: y })
+      } else if (e.type === 'mouseup' && e.button === 1) {
+        console.log({ type: 'mr', x: x, y: y })
+      } else if (e.type === 'mouseup' && e.button === 2) {
+        console.log({ type: 'rr', x: x, y: y })
+      }
+    }
 
-    return { translateX, translateY, gameWidth, gameHeight, getCellImg, getTranslateX, getTranslateY }
+    return { translateX, translateY, gameWidth, gameHeight, getCellImg, getTranslateX, getTranslateY, boardElement, cellMouseHandler }
   }
 })
 </script>
