e15254acbd6b1beac6c8bf5a56c746375b88d447
Flop <kukuzuo@gmail.com>
2021-12-04 17:04:36 +0800

    fix: 修复游戏区域部分缩放比例下背景有白边的问题

--- src/components/game/GameMain.vue
+++ src/components/game/GameMain.vue
@@ -1,12 +1,12 @@
 <template>
-  <!-- 背景颜色，用于修正部分缩放比例下有白边的问题，7.89 是为了填充游戏区域顶部和左侧与边框之前的白边，也是为了尽量不影响正常的图片显示 -->
-  <path :d="`M -7.89 -7.89 h ${gameWidth * 160 + 7.89} v 15.78 h ${gameWidth * -160 + 7.89} v ${gameHeight * 160 - 7.89} h -15.78 Z`" fill="gray" />
-  <!-- 方块容器，除了方块不要放其他元素，避免鼠标事件的坐标位置获取错误 -->
-  <g ref="boardElement" :transform="`translate(${translateX} ${translateY})`">
+  <g :transform="`translate(${translateX} ${translateY})`">
+    <!-- 背景颜色，用于修正部分缩放比例下有白边的问题，7.89 是为了填充游戏区域顶部和左侧与边框之前的白边，也是为了尽量不影响正常的图片显示，此元素不能放到方块容器外面，否则无法生效 -->
+    <path :d="`M -7.89 -7.89 h ${gameWidth * 160 + 7.89} v 15.78 h ${gameWidth * -160 + 7.89} v ${gameHeight * 160 - 7.89} h -15.78 Z`" fill="gray" />
     <template v-for="(item, height) in gameHeight" :key="item">
       <skin-symbol
         v-for="(item, width) in gameWidth"
         :key="item"
+        :ref="el => { if (el) cells[width + height * gameWidth] = el.$el }"
         :name="getCellImg(width, height)"
         :onmousedown="cellMouseHandler"
         :onmouseup="cellMouseHandler"
@@ -52,17 +52,17 @@ export default defineComponent({
     const getTranslateY = (height: number): number => {
       return height * CELL_SIDE_LENGTH * SVG_SCALE
     }
-    // 棋盘对应的元素
-    const boardElement = ref<SVGPolylineElement>()
+    // 棋盘方块对应的元素数组
+    const cells = ref<(SVGUseElement | undefined)[]>([])
     // 处理方块的鼠标事件，TODO 完善鼠标事件处理，除了添加鼠标移动事件还要在 document 上监听鼠标事件、添加移动端的点击事件处理
     const cellMouseHandler = (e: MouseEvent) => {
-      if (!boardElement.value) return
+      if (!cells.value[0]) return
       // 阻止默认的点击事件执行
       e.preventDefault()
       // 阻止捕获和冒泡阶段中当前事件的进一步传播
       e.stopPropagation()
-      // 获取方块容器的位置信息
-      const rect = boardElement.value.getBoundingClientRect()
+      // 获取左上角第一个方块的位置信息
+      const rect = cells.value[0].getBoundingClientRect()
       // 在方块上点击时，限制横坐标最小值为 0，最大值为 width - 1，因为原始数据可能已经被四舍五入过，点击边缘位置时计算得到的值可能超出实际游戏范围
       const x = Math.max(0, Math.min(round(e.x - rect.x, 0), store.state.width * 16 - 1))
       // 在方块上点击时，限制纵坐标最小值为 0，最大值为 height - 1
@@ -84,7 +84,7 @@ export default defineComponent({
       }
     }
 
-    return { translateX, translateY, gameWidth, gameHeight, getCellImg, getTranslateX, getTranslateY, boardElement, cellMouseHandler }
+    return { translateX, translateY, gameWidth, gameHeight, getCellImg, getTranslateX, getTranslateY, cells, cellMouseHandler }
   }
 })
 </script>
