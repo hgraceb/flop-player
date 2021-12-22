6263440328a7a0ea9d4df0d7ce4acae9fcfc711f
Flop <kukuzuo@gmail.com>
2021-12-22 20:32:51 +0800

    feat: 完善页面加载完成的通知

--- src/components/IframeAdapter.vue
+++ src/components/IframeAdapter.vue
@@ -6,8 +6,8 @@
 import { computed, defineComponent, Ref, ref, watch } from 'vue'
 import { store } from '@/store'
 import ScreenCenter from '@/components/common/ScreenCenter.vue'
-import { BaseParser } from '@/game/BaseParser'
 import { Share } from '@/game/constants'
+import { BaseParser } from '@/game/BaseParser'
 
 export default defineComponent({
   components: { ScreenCenter },
@@ -25,7 +25,7 @@ export default defineComponent({
       // 如果没有配置分享链接参数
       if (!share?.uri) return
       // 页面路径
-      const path = `${parent.window.location.origin}${share.pathname ? share.pathname : '/'}`
+      const path = `${parent.location.origin}${share.pathname ? share.pathname : '/'}`
       try {
         // 使用 base-64 编码对分享参数进行简单加密，可以一定程度避免参数被篡改，如：anonymous
         // 也可以避免部分浏览器在处理链接时会将多个 “.” 合并为一个 “.” 的问题，如：Quark 5.3.8.193 (Android)
@@ -42,42 +42,41 @@ export default defineComponent({
       }
     }
     // 暴露接口给父窗口
-    parent.window.flop = {
-      /** 播放录像 */
-      playVideo: (uri: string, { anonymous, background, share, listener }: { anonymous?: boolean, background?: string, share?: Share, listener?: () => void }) => {
-        // 设置页面退出状态以显示播放页面
-        store.commit('setExit', false)
-        // 设置可选参数
-        maskBackground.value = background || ''
-        exitListener.value = listener || (() => ({}))
-        store.commit('setAnonymous', anonymous)
-        // 拉取录像并播放
-        store.dispatch('fetchUri', uri)
-        // 设置分享链接
-        setShareLink(share)
-      },
-      /** 解析录像文件 */
-      parseFiles: (fileList: FileList | undefined | null, onload: (video: BaseParser) => void, onerror?: (info: string) => void) => {
-        onerror = onerror || (() => ({}))
-        store.dispatch('parseFiles', { fileList, onload, onerror })
-      }
+    parent.flop = parent.flop || {}
+    // 播放录像文件
+    parent.flop.playVideo = (uri: string, { anonymous, background, share, listener }: { anonymous?: boolean, background?: string, share?: Share, listener?: () => void }) => {
+      // 设置页面退出状态以显示播放页面
+      store.commit('setExit', false)
+      // 设置可选参数
+      maskBackground.value = background || ''
+      exitListener.value = listener || (() => ({}))
+      store.commit('setAnonymous', anonymous)
+      // 拉取录像并播放
+      store.dispatch('fetchUri', uri)
+      // 设置分享链接
+      setShareLink(share)
     }
+    // 解析录像文件
+    parent.flop.parseFiles = (fileList: FileList | undefined | null, onload: (video: BaseParser) => void, onerror?: (info: string) => void) => {
+      onerror = onerror || (() => ({}))
+      store.dispatch('parseFiles', { fileList, onload, onerror })
+    }
+    // 通知父窗口当前页面已经加载完成
+    if (parent.flop.onload) parent.flop.onload()
     // 退出时清理页面
     watch(computed(() => store.state.exit), (exit) => {
       if (exit) {
         exitListener.value()
         self.frameElement.classList.add(classDisplayNone)
-        parent.window.document.body.classList.remove(classOverflowHidden)
+        parent.document.body.classList.remove(classOverflowHidden)
       } else {
         self.frameElement.classList.remove(classDisplayNone)
-        parent.window.document.body.classList.add(classOverflowHidden)
+        parent.document.body.classList.add(classOverflowHidden)
       }
     }, {
       // 首次赋值时更新，方便在 iframe 中调试，避免热更新时无法退出游戏页面
       immediate: true
     })
-    // 通知父窗口当前页面已经加载完成
-    if (parent.flop.onload) parent.flop.onload()
     return { maskBackground }
   }
 })
